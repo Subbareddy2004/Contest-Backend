794085a2ed9d45d98d4a4edb047f512ba7416cb3
Tirumalareddysai <tirumalareddysai2004@gmail.com>
2024-11-02 14:20:44 +0530

    admin cors

--- src/index.js
+++ src/index.js
@@ -24,9 +24,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: [process.env.CLIENT_URL || 'http://localhost:5173', 'https://eyelabs.vercel.app'],
   credentials: true
 }));
+
 
 // Increase the payload size limit if needed
 app.use(express.json({ limit: '10mb' }));
